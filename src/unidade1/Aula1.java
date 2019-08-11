9c75acd14a5b35594674a3b604f3b246b5802063
Pedro Magalhaes <pedromagalhaes.hb@gmail.com>
2019-08-11 16:55:38 -0300

    Aula 1

---
+++ src/unidade1/Aula1.java
@@ -0,0 +1,18 @@
+package unidade1;
+
+import java.util.Scanner;
+
+public class Aula1 {
+
+    public static void main(String[] args) {
+        String message = "Olá, ";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String name = scanner.nextLine();
+
+        System.out.println(message + name);
+
+    }
+
+}
