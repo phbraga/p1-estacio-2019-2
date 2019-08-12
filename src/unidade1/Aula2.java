a9d28f74268385191fc5dfacca5f61cccde92918
Pedro Magalhaes <pedromagalhaes.hb@gmail.com>
2019-08-12 11:00:48 -0300

    Alterando aula 2

--- src/unidade1/Aula2.java
+++ src/unidade1/Aula2.java
@@ -3,6 +3,6 @@ package unidade1;
 public class Aula2 {
 
     public static void main (String[] args) {
-        System.out.println("Aula 2");
+        System.out.println("Alterando Aula 2");
     }
 }
